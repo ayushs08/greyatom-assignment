4e4aeae51123fee18673d2237d74dd64b9d09124
Ayush <ayushsindhwani08@gmail.com>
2019-09-20 10:10:06 +0530

    Integrate API

--- src/App.js
+++ src/App.js
@@ -6,79 +6,67 @@ import "./app.scss";
 
 class App extends React.Component {
   state = {
-    courses: [
-      {
-        img: "https://wallpaperaccess.com/full/99807.jpg",
-        title: "lorem ipsum dolor",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod vel sequi reiciendis possimus officiis aliquid ab? Dignissimos, quidem laboriosam saepe quasi totam repellendus ducimus harum eum, voluptas eius non eos?",
-        progress: 40
-      },
-      {
-        img: "https://wallpaperaccess.com/full/99807.jpg",
-        title: "lorem ipsum dolor",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-        progress: 40
-      },
-      {
-        img: "https://wallpaperaccess.com/full/99807.jpg",
-        title: "lorem ipsum dolor",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod vel sequi reiciendis possimus officiis aliquid ab? Dignissimos, quidem laboriosam saepe quasi totam repellendus ducimus harum eum, voluptas eius non eos?",
-        progress: 40
-      },
-      {
-        img: "https://wallpaperaccess.com/full/99807.jpg",
-        title: "lorem ipsum dolor",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod vel sequi reiciendis possimus officiis aliquid ab? Dignissimos, quidem laboriosam saepe quasi totam repellendus ducimus harum eum, voluptas eius non eos?",
-        progress: 40
-      },
-      {
-        img: "https://wallpaperaccess.com/full/99807.jpg",
-        title: "lorem ipsum dolor",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod vel sequi reiciendis possimus officiis aliquid ab? Dignissimos, quidem laboriosam saepe quasi totam repellendus ducimus harum eum, voluptas eius non eos?",
-        progress: 40
-      },
-      {
-        img: "https://wallpaperaccess.com/full/99807.jpg",
-        title: "lorem ipsum dolor",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod vel sequi reiciendis possimus officiis aliquid ab? Dignissimos, quidem laboriosam saepe quasi totam repellendus ducimus harum eum, voluptas eius non eos?",
-        progress: 40
-      }
-    ]
+    courses: [],
+    loading: true
   };
+
+  componentDidMount() {
+    fetch("http://api.myjson.com/bins/nxp6l")
+      .then(res => res.json())
+      .then(res => this.setState({ courses: res, loading: false }));
+  }
 
   render() {
     return (
       <div className="app">
-        <div className="grid">
-          {this.state.courses.map(({ img, title, description, progress }) => (
-            <Card className="course-card">
-              <Card.Header>
-                <img src={img} alt="" />
-              </Card.Header>
-              <Card.Body>
-                <div className="content">
-                  <div className="content__title">{title}</div>
-                  <div className="content__description">{description}</div>
-                  <div className="content__progress"></div>
-                </div>
-              </Card.Body>
-              <Card.Footer>
-                <button className="btn">
-                  {progress === 0
-                    ? "start"
-                    : progress < 100
-                    ? "continue"
-                    : "review"}
-                </button>
-              </Card.Footer>
-            </Card>
-          ))}
-        </div>
+        {this.state.loading ? (
+          <div className="loader"></div>
+        ) : (
+          <div className="grid">
+            {this.state.courses.map(
+              ({ image, title, description, progress }, index) => (
+                <Card className="course-card" key={index}>
+                  <Card.Header>
+                    <div className="ratio-container a24by9">
+                      <img src={image} alt="" />
+                    </div>
+                  </Card.Header>
+                  <Card.Body>
+                    <div className="content">
+                      <div className="content__title">{title}</div>
+                      <div className="content__description">{description}</div>
+                    </div>
+                    <div className="course-progress">
+                      <div className="d-flex">
+                        <div className="status">
+                          {progress === 0
+                            ? "not started"
+                            : progress + "% completed"}
+                        </div>
+                        <div className="actions"></div>
+                      </div>
+                      <div className="progress">
+                        <div
+                          className="progress__bar"
+                          style={{ width: progress + "%" }}
+                        ></div>
+                      </div>
+                    </div>
+                  </Card.Body>
+                  <Card.Footer>
+                    <button className="btn" onClick={() => alert(`${title}`)}>
+                      {progress === 0
+                        ? "start"
+                        : progress < 100
+                        ? "continue"
+                        : "review"}
+                    </button>
+                  </Card.Footer>
+                </Card>
+              )
+            )}
+          </div>
+        )}
       </div>
     );
   }
